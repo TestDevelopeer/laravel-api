f0b755800bc0bea48032da09047e3a0da3eebb4b
yessir <tdevelopeer@gmail.com>
2025-05-15 10:07:02 +0300

    Authentication and token generation

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(StoreUserRequest $request)
+    {
+        return User::create($request->all());
+    }
+
+    public function login(LoginUserRequest $request)
+    {
+        if (!Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return response()->json([
+                'message' => 'Wrong email or password'
+            ], 401);
+        }
+
+        $user = Auth::user();
+        $user->tokens()->delete();
+
+        return response()->json([
+            'user' => $user,
+            'access_token' => $user->createToken('Personal Access Token')->plainTextToken,
+        ]);
+    }
+
+    public function logout()
+    {
+
+    }
+}
